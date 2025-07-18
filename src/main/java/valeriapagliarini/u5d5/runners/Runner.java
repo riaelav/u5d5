5bacce6bc6adca8b84e078bf8481189914a2b1d6
Valeria Pagliarini <valepagliar@gmail.com>
2025-07-18 14:44:03 +0100

    save buildings test ok.

--- src/main/java/valeriapagliarini/u5d5/runners/Runner.java
+++ src/main/java/valeriapagliarini/u5d5/runners/Runner.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import valeriapagliarini.u5d5.entities.Building;
 import valeriapagliarini.u5d5.entities.User;
 import valeriapagliarini.u5d5.services.BuildingService;
 import valeriapagliarini.u5d5.services.UserService;
@@ -28,11 +29,26 @@ public class Runner implements CommandLineRunner {
         User user4 = new User("vhagar", "Aemond Targaryen", "aemond.targaryen@kingslanding.com");
         User user5 = new User("meleys", "Corlys Velaryon", "corlys.velaryon@kingslandin.com");
 
-        userService.saveUser(user1);
+       /*serService.saveUser(user1);
         userService.saveUser(user2);
         userService.saveUser(user3);
         userService.saveUser(user4);
-        userService.saveUser(user5);
+        userService.saveUser(user5);*/
+
+
+        Building building1 = new Building("Red Keep", "A castle in King’s Landing, seat of the Targaryen monarchy.", "King's Landing"
+        );
+        Building building2 = new Building("Dragonstone castle", "The Targaryen castle on a volcanic island", "Dragonstone");
+        Building building3 = new Building("Driftmark castle", "Island home of House Velaryon.", "Driftmark");
+        Building building4 = new Building("Castle Black", "The main stronghold of the Night's Watch", "The Wall");
+        Building building5 = new Building("Winterfell castle", "The ancient seat of House Stark", "Winterfell");
+
+       /* buildingService.saveBuilding(building1);
+        buildingService.saveBuilding(building2);
+        buildingService.saveBuilding(building3);
+        buildingService.saveBuilding(building4);
+        buildingService.saveBuilding(building5);*/
+
 
     }
 }
